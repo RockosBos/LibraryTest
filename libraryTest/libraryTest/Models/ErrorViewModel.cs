2c681a2ae599ef7f25155ef6c33fe0de1f8bbb68
Nick Kessey <nkessey@umich.edu>
2020-04-27 21:09:13 -0400

    initial template create

---
+++ libraryTest/libraryTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace libraryTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
